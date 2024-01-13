43fba7f635b9ac20c030d1f0f0ac3a4ac1fd45ed
REDOL <117319718+Kolge04@users.noreply.github.com>
2024-01-13 12:16:29 +0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-TOKEN = "5099605023:AAF5FFcl3iC0VKIl8vlN8rrB5Sxi8DHSI00"
+TOKEN = "6062022905:AAFjFdASrtYtMokfFwJwvtkgf0rhgG9Nlj0"
 with open('words.txt', 'r', encoding='utf-8') as file:
     word_list = file.read().splitlines()
